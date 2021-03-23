e3249359f96e76dc19c336af11f1ffc00499a593
vvvnik <vvv-nik@yandex.ru>
2021-03-23 12:52:16 +0300

    Change BOT_CONFIG

--- main.py
+++ main.py
@@ -10,42 +10,60 @@ BOT_CONFIG = {
         'bye': {
             'examples': ['Пока!', 'До свиданья!', 'Увидимся!!'],
             'responses': ['Чао!', 'Будь здоров', 'Сайонара']
+        },
+        'science': {
+            'examples': ['наука', 'исследования', 'учение'],
+            'responses': ['естествознание', 'образование', 'академия']
+        },
+        'century': {
+            'examples': ['век', 'эра', 'время'],
+            'responses': ['вечность!', 'жизнь', 'бесконечность']
+        },
+        'ice': {
+            'examples': ['gtr', 'мороз', 'лед'],
+            'responses': ['зима', 'холодильник', 'заморозки']
+        },
+        'water': {
+            'examples': ['дождь', 'жидкость', 'кипяток'],
+            'responses': ['вода', 'пар', 'сырость']
+        },
+        'space': {
+            'examples': ['dsf', 'космос', 'вселенная'],
+            'responses': ['высота', 'высь', 'небосвод']
+        },
+        'auto': {
+            'examples': ['авто', 'машина', 'транспорт'],
+            'responses': ['автомобиль', 'автобус', 'доставка']
         }
     },
     'default_answers': ['Извините, я тупой', 'Переформулируйте, меня еще не обучили']
 }  # "знания" бота
 
-
-def cleaner(text):  # функция очистки текста
+def cleaner(text): # функция очистки текста
     cleaned_text = ''
     for ch in text.lower():
-        if ch in 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя ':
+        if ch in ('абвгдеёжзийклмнопрстуфхцчшщъыьэюяqwertyuiopasdfghjklzxcvbnm'):
             cleaned_text = cleaned_text + ch
     return cleaned_text
 
-
-def match(text, example):  # гибкая функция сравнения текстов
+def match(text, example): # гибкая функция сравнения текстов
     return nltk.edit_distance(text, example) / len(example) < 0.4
 
-
-def get_intent(text):  # функция определения интента текста
+def get_intent(text): # функция определения интента текста
     for intent in BOT_CONFIG['intents']:
         for example in BOT_CONFIG['intents'][intent]['examples']:
-            if match(cleaner(text), cleaner(example)):
-                return intent
+             if match(cleaner(text), cleaner(example)):
+                  return intent
 
-
-def bot(text):  # функция бота
+def bot(text): # функция бота
     intent = get_intent(text)  # 1. попытаться понять намерение
     if intent is not None:
-        return random.choice(
-            BOT_CONFIG['intents'][intent]['responses'])  # 2. если удалось, ответить в соответствии намерением
+        return random.choice(BOT_CONFIG['intents'][intent]['responses']) # 2. если удалось, ответить в соответствии намерением
     else:
-        return random.choice(BOT_CONFIG['default_answers'])  # 3. если не удалось, ответить заглушкой
-
+        return random.choice(BOT_CONFIG['default_answers']) # 3. если не удалось, ответить заглушкой
 
 question = ''
 while question not in ['выход', 'выключайся']:
-    question = input('?-')
+    question = input()
     answer = bot(question)
     print(answer)
